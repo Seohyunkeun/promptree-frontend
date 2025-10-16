20c44bc645bd1b13807def54d204b2d8348e0001
Seohyunkeun <sidh0318@gmail.com>
2025-10-16 22:00:14 +0900

    fix: switch postcss config to .cjs and stage client changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css'        // <= 이 줄이 꼭 있어야 Tailwind 적용됨
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
